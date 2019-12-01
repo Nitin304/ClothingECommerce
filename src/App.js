8999aa79d846858240f918004c18454c6a1d594e
Nitin Arora <nitin.arora01@nagarro.com>
2019-12-01 11:52:16 +0530

    header

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import {Switch,Route} from 'react-router-dom';
 import {HomePage} from './pages/homePage/homePage.component';
 import Shop from './pages/shop-page/shop.component';
 
+import Header from './components/header/header.component';
+
 import './App.css'
 function App() {
   return (
-   <Switch>
-    <Route exact path="/" component={HomePage}></Route>
-    <Route exact path="/shop" component={Shop}></Route>
-   </Switch>
+    <div>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/shop" component={Shop}></Route>
+      </Switch>
+   </div>
   );
 }
 
---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './header.styles.scss';
+
+import {Link} from 'react-router-dom';
+import { ReactComponent as Logo} from '../../assets/crown.svg';
+
+const Header = ()=>(
+    <div className="header">
+        <Link to="/">
+            <Logo className="logo"></Logo>
+        </Link>
+        <div className="options">
+            <Link to="/shop" className="option">
+                SHOP
+            </Link>
+            <Link to="/contact" className="option">
+                CONTACT
+            </Link>
+        </div>
+    </div>
+)
+
+export default Header;
