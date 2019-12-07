24dbbe737a957a6bdface26b7dbc6ee15aa2cb9d
Nitin Arora <nitin.arora01@nagarro.com>
2019-12-07 13:23:22 +0530

    Sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import {signInWithGoogle} from '../../firebase/firebase.util';
+import {signInWithGoogle,auth} from '../../firebase/firebase.util';
 
 
 class SignIn extends React.Component{
@@ -16,9 +16,16 @@ class SignIn extends React.Component{
             password:''
         }
     }
-    handleSubmit = event =>{
+    handleSubmit = async event =>{
         event.preventDefault();
-        this.setState({email:'',password:''});
+        const {email, password} = this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({email:'',password:''});
+        }
+        catch(e){
+            alert(e.message);
+        }        
     }
     handleChange = event =>{
         const {value,name} = event.target;
