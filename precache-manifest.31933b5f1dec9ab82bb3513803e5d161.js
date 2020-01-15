d1f93a4573e37fb7c673286eccef2a90407cebb3
Nitin Arora <nitin.arora01@nagarro.com>
2020-01-15 10:51:38 +0530

    Updates

---
+++ precache-manifest.31933b5f1dec9ab82bb3513803e5d161.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0269db12009d4932a002ac49e64ef54",
+    "url": "/ClothingECommerce/index.html"
+  },
+  {
+    "revision": "eca802dfccd3a55c720f",
+    "url": "/ClothingECommerce/static/css/main.16d2481f.chunk.css"
+  },
+  {
+    "revision": "b4a16f0855d23f30ba8c",
+    "url": "/ClothingECommerce/static/js/2.f11c577a.chunk.js"
+  },
+  {
+    "revision": "eca802dfccd3a55c720f",
+    "url": "/ClothingECommerce/static/js/main.971ee867.chunk.js"
+  },
+  {
+    "revision": "3021eea1d19138867124",
+    "url": "/ClothingECommerce/static/js/runtime-main.2d80533a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/ClothingECommerce/static/media/crown.ed28accb.svg"
+  }
+]);
